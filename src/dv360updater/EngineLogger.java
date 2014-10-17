3f55328c89be7789a8fe760116e1d6189a7ca3ea
ArturoMartinez <arturo.martinez@gdsmodellica.com>
2014-10-17 15:07:44 +0200

    [2014/10/17] - Added own logger formatter.

--- src/dv360updater/EngineLogger.java
+++ src/dv360updater/EngineLogger.java
@@ -2,10 +2,42 @@ package dv360updater;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+
+
+class EngineLoggerFormatter extends Formatter{
+
+    private final DateFormat df = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS");
+
+    @Override
+    public String format(LogRecord record) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(df.format(new Date(record.getMillis()))).append(" - ");
+        //builder.append("[").append(record.getSourceClassName()).append(".");
+        //builder.append(record.getSourceMethodName()).append("] - ");
+        builder.append("[").append(record.getLevel()).append("] :: ");
+        builder.append(formatMessage(record));
+        builder.append("\n");
+        return builder.toString();
+    }
+
+    @Override
+    public String getHead(Handler h){
+        return super.getHead(h);
+    }
+    @Override
+    public String getTail(Handler h){
+        return super.getTail(h);
+    }
+}
 
 /**
  * @version 1.0.00
@@ -13,15 +45,16 @@ import java.util.logging.Logger;
  */
 public class EngineLogger {
     
-    private static final Logger _log = Logger.getAnonymousLogger();
+    private static final Logger _log = Logger.getLogger("DV360 Workbench Updater");
     
-    public EngineLogger(){
-        _log.setLevel(Level.ALL);
-    }
+    public EngineLogger(){}
     
     public EngineLogger(String exePath) throws IOException{
         this();
-        _log.addHandler(new FileHandler(new File(exePath).getParentFile().getAbsolutePath()+File.separator+"logs"+File.separator+"dv360updater.log"));
+        Handler hndlr = new FileHandler(new File(exePath).getParentFile().getAbsolutePath()+File.separator+"logs"+File.separator+"dv360updater%g.log", false);
+        hndlr.setFormatter(new EngineLoggerFormatter());
+        _log.addHandler(hndlr);
+        _log.setLevel(Level.ALL);
     }
     
     public void info(String message){
